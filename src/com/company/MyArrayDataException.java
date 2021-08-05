b9abdd1b8ba58a5ba6bb933ef05d90e89fc08880
Egor <sokolovegor1998@gmail.com>
2021-08-06 00:26:33 +0300

    Initial commit

---
+++ src/com/company/MyArrayDataException.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MyArrayDataException extends  NumberFormatException{
+
+    private int row;
+    private int column;
+    private Object value;
+
+    public MyArrayDataException(int row, int column, Object [][] array) {
+        super("Одна ошибка и ты ошибся в этой ячейке неправильный тип данных. Invalid  data in array["+ row +"]["+ column +"] Неправильный элемент="  + array[row][column] );
+        this.row = row;
+        this.column = column;
+        this.value = array[row][column];
+    }
+}
---
+++ src/com/company/MyArraySizeException.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class MyArraySizeException extends RuntimeException {
+
+
+
+
+    public MyArraySizeException() {
+        super("Одна ошибка и ты ошибся. Неправильный размер массива" );
+
+    }
+}
